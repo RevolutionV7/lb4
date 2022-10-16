d5143a31d1a199fd9ee4c390c14170c75005a924
RevolutionV7 <sdvsvd11@gmail.com>
2022-10-16 22:45:34 +0300

    First progr finish

---
+++ Завдання 1/ConsoleApp1/ConsoleApp1/Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace DebugOne
+{
+    internal class Task1<T>
+    {
+        public void GetInDebugMethod(T first, T second)
+        {           
+            Debug.WriteLine(first);
+            Debug.WriteLine(second);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"Type for first parameter is : {first.GetType()} and value :  ");
+            Console.WriteLine(first);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"Type for second parameter is : {second.GetType()} and value :  ");
+            Console.WriteLine(second);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
